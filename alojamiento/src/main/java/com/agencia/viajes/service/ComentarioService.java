4b499ed0b00198ffc6e6845b20e02aa0ec316f5c
DidierLLantenPc <didiervelez@hotmail.es>
2024-05-16 01:13:00 -0500

    RabbitMQ para validar el id de la persona responsable del COMENTARIO

--- alojamiento/src/main/java/com/agencia/viajes/service/ComentarioService.java
+++ alojamiento/src/main/java/com/agencia/viajes/service/ComentarioService.java
@@ -1,20 +1,26 @@
 package com.agencia.viajes.service;
 
+import com.agencia.viajes.config.Constantes;
 import com.agencia.viajes.model.Comentario;
 import com.agencia.viajes.repository.IComentarioJPARepository;
 import com.agencia.viajes.service.interfaces.IComentarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class ComentarioService implements IComentarioService {
     private final IComentarioJPARepository comentarioJPARepository;
 
+    private final RabbitTemplate rabbitTemplate;
+
     @Override
     public Comentario createComentario(Comentario comentario) {
+        validarPersona(comentario.getIdAutor());
         return comentarioJPARepository.save(comentario);
     }
 
@@ -26,5 +32,18 @@ public class ComentarioService implements IComentarioService {
     @Override
     public List<Comentario> getAlojamientosByIdAutor(int idAutor) {
         return comentarioJPARepository.findComentariosByIdAutor(idAutor);
+    }
+
+    private void validarPersona(int idPersona){
+        Object respuesta = rabbitTemplate.convertSendAndReceive(Constantes.EXCHANGE, Constantes.ROUTING_KEY, idPersona);
+
+        if(Objects.isNull(respuesta)){
+            throw new RuntimeException("Hubo un error recuperando la información de la persona");
+        }
+
+        boolean existe = (Boolean) respuesta;
+        if(!existe){
+            throw new RuntimeException("La persona con id: "+idPersona+" no existe");
+        }
     }
 }
